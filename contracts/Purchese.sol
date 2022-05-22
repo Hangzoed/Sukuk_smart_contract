f07d17f9bf09363427dcc3f628e595d2a0f08175
Hangzoed <omeradelez@gmail.com>
2022-05-22 10:51:08 +0400

    first commit

---
+++ contracts/Purchese.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+
+contract Sukuk{
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public investors;
+    address public admin;
+    AggregatorV3Interface public priceFeed;
+
+    //added an argument to constructor for testing
+    constructor(address _priceFeed  ) public{
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        // Admin will be the contract sender for now
+        admin = msg.sender;
+
+    }
+
+    function fund() public payable{
+
+
+
+        // Right now it only tracks the amount and address only.
+        addressToAmountFunded[msg.sender] += msg.value;
+        investors.push(msg.sender);
+    }
+
+
+    modifier onlyAdmin(){
+        require(msg.sender == admin);
+        _;
+    }
+
+    function withdraw() public payable onlyAdmin{
+        msg.sender.transfer(address(this).balance);
+
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < investors.length;
+            funderIndex++
+        ) {
+            address funder = investors[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        investors = new address[](0);
+    }
+
+
+    }
+
+
